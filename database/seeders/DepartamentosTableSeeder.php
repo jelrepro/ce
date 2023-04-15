2b6c3edf9d46c5d8da80a911e9717d6e47259862
jalex <jalexmamian1234@gmail.com>
2023-04-14 20:49:02 -0500

    project start

---
+++ database/seeders/DepartamentosTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DepartamentosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('departamentos')->insert([
+            'codigo' => '19',
+            'descripcion' => 'CAUCA',
+        ]);
+        DB::table('departamentos')->insert([
+            'codigo' => '52',
+            'descripcion' => 'NARIÑO',
+        ]);
+        DB::table('departamentos')->insert([
+            'codigo' => '76',
+            'descripcion' => 'VALLE DEL CAUCA',
+        ]);
+    }
+}
