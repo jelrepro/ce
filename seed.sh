115edf93e2a3980f6201efc5a08c147790db2a40
davidpinchao <ldpinchao1@gmail.com>
2023-04-15 15:37:11 -0500

    seeder

--- seed.sh
+++ seed.sh
@@ -5,6 +5,9 @@ php artisan db:seed --class=DepartamentosTableSeeder
 php artisan db:seed --class=MunicipiosTableSeeder
 php artisan db:seed --class=EstadosGestionTableSeeder
 php artisan db:seed --class=EstadosAdminTableSeeder
+php artisan db:seed --class=GrupoProyectosTableSeeder
+php artisan db:seed --class=TipoProyectosTableSeeder
+php artisan db:seed --class=ProyectosTableSeeder
 # php artisan db:seed --class=ConveniosTableSeeder
 # php artisan db:seed --class=ProyectosTableSeeder
 
