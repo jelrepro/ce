c81a17c7333e8b543025aa541fbbd14e70318d8e
jalex <jalexmamian1234@gmail.com>
2023-04-15 21:07:10 -0500

    prueba 12

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'CEO') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased bg-gray-50">
+        <x-banner />
+
+
+
+        @php
+            $links = [
+                [
+                    'title' => 'Dashboard',
+                    'url' => route('admindashboard'),
+                    'active' => request()->routeIs('admindashboard'),
+                    'icon' => 'fa-solid fa-gauge-high',
+                ],
+                
+
+            ];
+
+        @endphp
+
+        <div class="flex" x-data="{
+            open: false,
+            openSidebar: true
+        }">
+
+            <div :class="{
+                'lg:block': openSidebar,
+            }" class="w-64 flex-shrink-0 hidden lg:block">
+
+                @include('layouts.partials.admin.sidebar')
+
+            </div>
+            <div class="flex-1">
+                @include('layouts.partials.admin.navigation')
+
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+
+
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
+</html>
--- resources/views/layouts/partials/admin/navigation.blade.php
+++ resources/views/layouts/partials/admin/navigation.blade.php
@@ -1,25 +1,20 @@
-@php
-    $links = [
-        [
-            'title' => 'Home',
-            'url' => route('home'),
-            'active' => request()->routeIs('home'),
-        ],
-        [
-            'title' => 'Administrador',
-            'url' => route('admindashboard'),
-            'active' => request()->routeIs('admindashboard'),
-        ]
-
-    ];
-
-@endphp
-
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+
+            <div class="-mr-2 lg:flex items-center hidden">
+                <button @click="openSidebar = ! openSidebar"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </div>
+
+            <div class="flex lg:hidden">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="/">
@@ -28,17 +23,9 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    @foreach ($links as $link)
-                        <x-nav-link href="{{ $link['url'] }}" :active="$link['active']">
-                            {{ $link['title'] }}
-                        </x-nav-link>
-                    @endforeach
-                </div>
             </div>
 
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden lg:flex sm:items-center sm:ml-6">
                 @auth
 
                     <!-- Settings Dropdown -->
@@ -114,7 +101,7 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
+            <div class="-mr-2 flex items-center lg:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
@@ -129,7 +116,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden">
         <div class="pt-2 pb-3 space-y-1">
 
             @foreach ($links as $link)
